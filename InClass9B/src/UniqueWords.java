d4e1e5ba6b32095f84dea972b76e4a55c2fbd0e7
sluong18-occ <54555938+sluong18-occ@users.noreply.github.com>
2019-10-23 13:19:39 -0700

    Add files via upload

---
+++ InClass9B/src/UniqueWords.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * How many unique words in Alice in Wonderland?
+ * Complete the program to find out.
+ * 
+ * @author 
+ * @version 
+ *
+ */
+public class UniqueWords
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {        
+        Scanner in = new Scanner(new File("alice30.txt"));
+        
+        /* TODO: Get the appropriate data structure ready... */
+        Set<String> allwords = new HashSet<String>();
+        
+        while (in.hasNext())
+        {
+            /* TODO: Read and process the next word... */
+            allwords.add(in.next().toLowerCase());
+        }
+        in.close();
+        /* TODO: Print the answer... */ 
+        System.out.println("The number of unique words in Alice in Wonderland: ");
+        System.out.println(allwords.size());
+    }
+}
+
